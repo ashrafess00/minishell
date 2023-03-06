6cd32b5b0afe27fe2121c64ae3468e5b78a2c69d
Achraf Essaoudi <aessaoud@c2r5p3.1337.ma>
2023-03-06 20:57:50 +0100

    the war is begin

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <termios.h>
+
+int main(void)
+{
+    printf("hello Ashraf and Khalid");
+}
