209cb2a7b182de996e703aeff1333a4befbdacca
Khalid Slik <kslik@c1r6p1.1337.ma>
2023-03-30 16:40:24 +0000

    test

--- mini.h
+++ mini.h
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   mini.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kslik <kslik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/30 16:38:09 by kslik             #+#    #+#             */
-/*   Updated: 2023/03/30 16:40:04 by kslik            ###   ########.fr       */
+/*   Created: 2023/03/30 16:38:22 by kslik             #+#    #+#             */
+/*   Updated: 2023/03/30 16:39:44 by kslik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mini.h"
+#ifndef MINI_H
+#define MINI_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <termios.h>
 
-int main(void)
-{
-    printf("bda lkhdma ya lqsir");
-}
+#endif
