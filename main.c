46b67cd38987029dd0213ee8ac9a96579a16612e
Khalid Slik <kslik@c1r6p1.1337.ma>
2023-03-31 13:35:48 +0000

    s

--- main.c
+++ main.c
@@ -6,14 +6,29 @@
 /*   By: kslik <kslik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 16:38:09 by kslik             #+#    #+#             */
-/*   Updated: 2023/03/30 16:40:04 by kslik            ###   ########.fr       */
+/*   Updated: 2023/03/31 13:27:57 by kslik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <termios.h>
 
 
-int main(void)
+int main()
 {
-    printf("bda lkhdma ya lqsir");
+    char *line;
+    write(1, "our shell >", 13);
+    // while(1)
+    line = readline("");
+    printf("%s",line);
+    return(0);
 }
