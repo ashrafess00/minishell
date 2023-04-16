83b73cb9071ee6d32f8821c2c9c73d1c0f0fe118
Achraf Essaoudi <aessaoud@c2r5p3.1337.ma>
2023-04-16 20:51:05 +0000

    tokenization is done, line kayt9ssm okolla token 3ndo smito

--- srcs/check_tokens.c
+++ srcs/check_tokens.c
@@ -6,124 +6,39 @@
 /*   By: aessaoud <aessaoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 19:47:49 by aessaoud          #+#    #+#             */
-/*   Updated: 2023/04/12 18:07:59 by aessaoud         ###   ########.fr       */
+/*   Updated: 2023/04/16 20:49:50 by aessaoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-int	is_pipe(char *s, char q, int *error)
+int	is_env(t_token **head,  char *line, int *i, int *s_index, int size, char *q)
 {
-	int	count;
-	int	i;
-
-	count = 0;
-	i = 0;
-	if (s[0] == '|')
+	if ((*q == 'c' || *q == '\"') && line[*i] == '$' && line[*i + 1] == '?')
 	{
-		count += 1;
-		while (s[++i])
-		{
-			if (s[i] == '|')
-				count ++;
-			if (count == 2 && q == 'c')
-			{
-				printf("syntax error : unexpected token %c\n", s[i]);
-				*error = 1;
-				return (0);
-			}
-			if (s[i] != ' ')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-int is_heredoc(char *s, char q, int *error)
-{
-	int count;
-	int i;
-
-	i = 1;
-	
-	if (s[0] == '<' && s[1] == '<')
-	{
-		count = 2;
-		while (s[++i])
-		{
-			if (s[i] == '<' || s[i] == '>' || s[i] == '|')
-				count++;
-			if (count > 2)
-			{
-				printf("syntax error : unexpected token [d%c\n", s[i]);
-				
-				*error = 1;
-				return (0);
-			}
-			if (s[i] != ' ')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-int	is_red_input_append(char *s, char q, int *error)
-{
-	int count;
-	int i;
-
-	i = 1;
-	
-	if (s[0] == '>' && s[1] == '>')
-	{
-		count = 2;
-		while (s[++i])
-		{
-			if (s[i] == '<' || s[i] == '>' || s[i] == '|')
-				count++;
-			if (count > 2)
-			{
-				printf("syntax error : unexpected token %c\n", s[i]);
-				*error = 1;
-				return (0);
-			}
-			if (s[i] != ' ')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-int is_red_output(char *s, char q, int *error)
-{
-	int	count;
-	int	i;
-
-	i = -1;
-	if (s[0] == '>' && s[1] == '|')
-	{
-		*error = 1;
-		printf("syntax error : unexpected token %c\n", s[i]);
-		return (0);
-	}
-	else if (s[0] == '>')
+		cr_token(head, line, *s_index, size, NORMAL);
+		cr_token(head, line, *i, 2, EXIT_STATUS);
+		*i += 1;
 		return (1);
-	return (0);
-}
-
-int is_red_input(char *s, char q, int *error)
-{
-	int	count;
-	int	i;
-
-	i = -1;
-	if (s[0] == '<' && s[1] == '|')
-	{
-		*error = 1;
-		printf("syntax error : unexpected token %c\n", s[i]);
-		return (0);
 	}
-	else if (s[0] == '<')
+	if ((*q == 'c' || *q == '\"') && line[*i] == '$')
+	{
+		int si;
+	
+		si = 0;
+		cr_token(head, line, *s_index, size, NORMAL);
+		*s_index = *i;
+		*i += 1;
+		while (line[*i] != ' ' && line[*i] != '$' && line[*i] != '>'
+		&& line[*i] != '<' && line[*i] != D_QUOTE
+		&& line[*i] != S_QUOTE && line[*i])
+		{
+			si++;
+			*i += 1;
+		}
+		cr_token(head, line, *s_index, si + 1, ENV);
+		*i -= 1;
 		return (1);
+	}
 	return (0);
 }
