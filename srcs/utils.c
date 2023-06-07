f57a41975f92f5fe19982eac1658c08680ff6129
Achraf Essaoudi <aessaoud@c2r5p1.1337.ma>
2023-06-07 16:53:32 +0100

    working on signal on herdoc

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: aessaoud <aessaoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 17:58:16 by aessaoud          #+#    #+#             */
-/*   Updated: 2023/05/25 13:09:21 by aessaoud         ###   ########.fr       */
+/*   Updated: 2023/06/07 16:52:33 by aessaoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,26 +32,57 @@ int	ft_strcmp(char *s, char *limiter)
 	return (0);
 }
 
+void	nono(int signum)
+{
+	if (signum == SIGINT)
+	{
+		write(1, "\n", 1);
+		exit(0);
+	}
+}
+
 char	*get_input_from_usr(char *limiter, t_my_env *my_env)
 {
 	char	*input;
 	char	*s;
+	int		pid;
+	int		fds[2];
+	int		status;
 
 	input = ft_calloc(1, 1);
-	while (1)
+	signal(SIGINT, SIG_IGN);	
+	pid = fork();
+	if (pid == 0)
 	{
-		ft_printf("> ");
-		s = get_next_line(0);
-		if (s)
-			s = expandini(s, my_env, 0);
-		if (!s)
-			break ;
-		if (!ft_strcmp(s, limiter))
-			break ;
-		input = ft_strjoin(input, s);
+		signal(SIGINT, SIG_DFL);
+		close(fds[0]);
+		while (1)
+		{
+			ft_printf("> ");
+			s = get_next_line(0);
+			if (s)
+				s = expandini(s, my_env, 0);
+			if (!s)
+				return NULL ;
+			if (!ft_strcmp(s, limiter))
+				break ;
+			input = ft_strjoin(input, s);
+		}
+		write(fds[1], input, ft_strlen(input));
+		close(fds[1]);
+		free(s);
+		exit(0);
 	}
-	free(s);
-	return (input);
+	else
+	{
+		signal(SIGINT, ctrl_c_handler);
+		close(fds[1]);
+		waitpid(pid, &status, 0);
+		read(fds[0], input, 1000);
+		close(fds[0]);
+		return (input);
+	}
+	return (NULL);
 }
 
 char	*get_cdir(int exit_code)
